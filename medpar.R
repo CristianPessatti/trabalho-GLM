394c0a8551f561793fd3846829ee838232478bc0
VitorKiemo <115746309+VitorKiemo@users.noreply.github.com>
2024-11-07 20:56:46 -0300

    Alterações básicas.

--- medpar.R
+++ medpar.R
@@ -1,4 +1,4 @@
-# Instruções ======================================================================================
+### Instruções ====================================================================================
 # A análise deverá ser baseada no ajuste de um modelo log-linear de Poisson. Modelos de regressão 
 # alternativos, como a regressão com resposta binomial negativa e modelos de quaseverossimilhança deverão
 # ser considerados para acomodar possível superdispersão.
@@ -17,8 +17,9 @@
 require(tidyverse)
 require(msme)
 require(DescTools)
+require(GGally)
 
-# Definição do problema e descrição resumida dos dados e das variáveis consideradas ===============
+### Definição do problema e descrição resumida dos dados e das variáveis consideradas =============
 data(medpar)
 dados <- medpar[c("los", "hmo", "white", "age80", "type")]
 # A base consiste em informações hospitalares do Arizona, referentes a uma parcela de pacientes de um grupo
@@ -29,13 +30,16 @@ dados <- medpar[c("los", "hmo", "white", "age80", "type")]
 # - age80: se o paciente possui 80 anos ou mais ou menos de 80 anos; 0 = <80, 1 = >=80;
 # - type: tipo de admissão; (variável categórica)
 
-# Análise descritiva/exploratória =================================================================
+### Análise descritiva/exploratória ===============================================================
 dados$hmo <- as.factor(dados$hmo)
 dados$white <- as.factor(dados$white)
 dados$age80 <- as.factor(dados$age80)
 dados$type <- as.factor(dados$type)
 
+ggpairs(dados) + theme_bw()
+# Podemos ver que as variáveis explicativas são bastante desbalanceadas. 
 
+### Especificação do modelo ajustado ==============================================================
 # Ajuste do modelo Poisson com ligação log
 mod1 <- irls(los ~ hmo + white + age80 + type, family = "poisson", link = "log", data = dados)
 summary(mod1)
